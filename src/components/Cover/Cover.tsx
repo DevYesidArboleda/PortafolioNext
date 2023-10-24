b7ea40954298c5da12e8f54f27d3e9e56e63f247
DevYesidArboleda <yesison714@gmail.com>
2023-10-24 16:30:48 -0500

    Cover y header

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,7 +1,19 @@
-
+"use client"
+import { useCallback } from "react"
+import Particles from "react-tsparticles"
+import { loadFull } from "tsparticles"
+import type { Engine } from "tsparticles-engine"
+import { optionsParticles } from "./particleOptions"
 
 export function Cover() {
+    const particlesInit = useCallback(async (engine:Engine)=>{
+        await loadFull(engine)
+    }, [])
+
+    const particlesLoaded = useCallback(async ()=>{}, [])
     return (
-        <div>Cover</div>
+        <div id="cover">
+
+        </div>
     )
 }
